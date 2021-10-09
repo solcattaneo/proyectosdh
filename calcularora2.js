753632cf081f630704ff3df5a7fb00460c48f428
leo <lylcomputacion@live.com>
2021-10-09 20:51:38 -0300

    mi primer commit

---
+++ calcularora2.js
@@ -0,0 +1,7 @@
+let sumar = (num1 , muu2)=> num1 + num2
+let restar = (num1 , mun2)=> num1 - num2
+let dividir = (num1 , mun2)=> num1 / num2
+let multiplicar = (num1 , mun2)=> num1 * num2
+let calculadora = (num1,num2,operador)=> operador (num1,num2)
+
+console.log (calculadora (18,3,sumar));
