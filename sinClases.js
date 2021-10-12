b5c51ece1bd15a38302130ac14319e8efa97c784
leo <lylcomputacion@live.com>
2021-10-12 10:20:47 -0300

    buenos dias

---
+++ sinClases.js
@@ -0,0 +1,9 @@
+let tengoClases="domingo" 
+	switch (tengoClases) {
+		case "lunes","miercoles","viernes":
+		console.log ( "tenés clases")
+		break;
+		case  "martes","jueves","sabado","domingo":
+		console.log ("no tenés clases")
+		break;
+    }
