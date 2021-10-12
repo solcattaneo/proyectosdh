aef28e6cc50a6884ca703f4d12ace97026458704
leo <lylcomputacion@live.com>
2021-10-11 22:06:56 -0300

    cambios

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+const sumar = require('./ sumar ')
+const restar = require('./restar ')
+const dividir = require('./ dividir ')
+const multiplicar = require('./ multiplicar ')
+
+
+console.log ('sumar', sumar (4,5))
+console.log ('restar', restar (4,5))
+console.log ('multiplicar', multiplicar(4,5))
+console.log ('dividir', dividir(4,5))
+
+///console.log  github
